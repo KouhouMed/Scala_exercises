a51cd1f96508eb787abc1ebb223bc895bb721fb1
KouhouMed <mohamedkouhou@gmail.com>
2024-07-28 13:50:18 +0200

    largest element

--- src/main/scala/com/mkouhou/controlflow/LargestElement.scala
+++ src/main/scala/com/mkouhou/controlflow/LargestElement.scala
@@ -5,5 +5,22 @@ package com.mkouhou.controlflow
 * */
 
 object LargestElement extends App {
+  def largest(arr: Array[Int]): Int = {
+    arr match {
+      case Array() => throw new NoSuchElementException("Array is empty")
+      case Array(x) => x
+      case Array(x, y) => x max y
+      case _ =>
+        arr.foldLeft(Int.MinValue) { (acc, x) =>
+          if (acc < x) x else acc
+        }
+    }
+  }
+
+  // test
+
+  val arr = Array(3, 1, 18, 1, 10, 5, 9, 2, 6, 80, 0)
+  val largestElement = largest(arr)
+  println(s"The largest element in the array is $largestElement")
 
 }
