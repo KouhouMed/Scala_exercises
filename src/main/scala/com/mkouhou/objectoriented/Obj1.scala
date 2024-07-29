07ea7979579296e0759b67fff064ef22d5d7314e
KouhouMed <mohamedkouhou@gmail.com>
2024-07-29 14:11:47 +0200

    object orientation exercises

---
+++ src/main/scala/com/mkouhou/objectoriented/Obj1.scala
@@ -0,0 +1,10 @@
+package com.mkouhou.objectoriented
+
+/*
+* Write a  Scala program that creates a class called Person with properties like name, age and country.
+* Implement methods to get and set properties.
+* */
+object Obj1 extends App {
+  class Person()
+
+}
