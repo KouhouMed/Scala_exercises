83eb07b1e0b8ad934e3fda1af665f05764705370
KouhouMed <mohamedkouhou@gmail.com>
2024-07-29 14:25:02 +0200

    object orientation exercises

--- src/main/scala/com/mkouhou/objectoriented/Obj1.scala
+++ src/main/scala/com/mkouhou/objectoriented/Obj1.scala
@@ -5,6 +5,17 @@ package com.mkouhou.objectoriented
 * Implement methods to get and set properties.
 * */
 object Obj1 extends App {
-  class Person()
+  class Person(var name: String, var age: Int, var country: String) {
+    def this() = this("", 0, "")
+
+    def getName: String = name
+    def setName(newName: String): Unit = name = newName
+
+    def getAge: Int = age
+    def setAge(newAge: Int): Unit = age = newAge
+
+    def getCountry: String = country
+    def setCountry(newCountry: String): Unit = country = newCountry
+  }
 
 }
