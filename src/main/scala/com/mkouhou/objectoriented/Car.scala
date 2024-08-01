ac2839d3b46bdc0a58e86a7b8e0a20f52aad3408
KouhouMed <mohamedkouhou@gmail.com>
2024-08-01 12:35:36 +0200

    car class

--- src/main/scala/com/mkouhou/objectoriented/Car.scala
+++ src/main/scala/com/mkouhou/objectoriented/Car.scala
@@ -2,6 +2,7 @@ package com.mkouhou.objectoriented
 
 /*
 * Write a Scala program that creates a class Car with properties like make, model, and year.
+* Implement a method to display car information.
 * */
 class Car {
 
