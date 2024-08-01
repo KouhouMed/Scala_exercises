92d2ef3b6552d4f5d407f245510e603fb0053d98
KouhouMed <mohamedkouhou@gmail.com>
2024-08-01 12:36:08 +0200

    car class

--- src/main/scala/com/mkouhou/objectoriented/Car.scala
+++ src/main/scala/com/mkouhou/objectoriented/Car.scala
@@ -4,6 +4,12 @@ package com.mkouhou.objectoriented
 * Write a Scala program that creates a class Car with properties like make, model, and year.
 * Implement a method to display car information.
 * */
-class Car {
-
+class Car(val make: String, val model: String, val year: Int) {
+  def displayInfo(): Unit = {
+    println(s"Car Information:")
+    println(s"Make: $make")
+    println(s"Model: $model")
+    println(s"Year: $year")
+    println("------------------------")
+  }
 }
