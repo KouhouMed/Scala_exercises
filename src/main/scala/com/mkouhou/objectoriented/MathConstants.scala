877b61970f5d9fab558da551868457a7f5796f2f
KouhouMed <mohamedkouhou@gmail.com>
2024-08-03 16:33:59 +0200

    math constants

---
+++ src/main/scala/com/mkouhou/objectoriented/MathConstants.scala
@@ -0,0 +1,13 @@
+package com.mkouhou.objectoriented
+
+/*
+* Write a Scala program that creates an object MathConstants with constants for mathematical calculations such as PI and E
+* */
+object MathConstants extends App {
+  val PI: Double = 3.141592653589793
+  val E: Double = 2.718281828459045
+  val GOLDEN_RATIO: Double = 1.618033988749895
+  val SQRT_2: Double = 1.414213562373095
+  val EULER_MASCHERONI: Double = 0.577215664901532
+
+}
