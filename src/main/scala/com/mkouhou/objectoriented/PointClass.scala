dc5026eb95a97cf4c2c216d24c38f9d8d86f74f4
KouhouMed <mohamedkouhou@gmail.com>
2024-08-04 23:15:20 +0200

    point

--- src/main/scala/com/mkouhou/objectoriented/PointClass.scala
+++ src/main/scala/com/mkouhou/objectoriented/PointClass.scala
@@ -4,3 +4,14 @@ package com.mkouhou.objectoriented
 * Write a Scala program that creates a class Point with properties x and y coordinates.
 * Use a destructuring declaration to extract the coordinates.
 * */
+
+object PointClass extends App{
+  def main(args: Array[String]): Unit = {
+    val point = Point(2, 1)
+
+    val Point(x, y) = point
+
+    println(s"x-coordinate: $x")
+    println(s"y-coordinate: $y")
+  }
+}
