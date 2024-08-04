c4f77af4c1ce365640b84e0c678f34aa7e0829a8
KouhouMed <mohamedkouhou@gmail.com>
2024-08-04 23:14:05 +0200

    point

---
+++ src/main/scala/com/mkouhou/objectoriented/PointClass.scala
@@ -0,0 +1,6 @@
+package com.mkouhou.objectoriented
+
+/*
+* Write a Scala program that creates a class Point with properties x and y coordinates.
+* Use a destructuring declaration to extract the coordinates.
+* */
